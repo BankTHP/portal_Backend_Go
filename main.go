fbb6516a93b8f99c210dbfae8f4dbdf656755a73
LAPTOP-TB43LSM8\PPC-Bank <bang.225588z@gmail.com>
2024-10-09 10:33:58 +0700

    setup project golang

---
+++ main.go
@@ -0,0 +1,29 @@
+﻿package main
+
+import (
+    "fmt"
+    "log"
+    "github.com/spf13/viper"
+)
+
+func initConfig() {
+    env := viper.GetString("GO_ENV")
+    if env == "" {
+        env = "dev"
+    }
+
+    viper.SetConfigName(fmt.Sprintf("config.%s", env))
+    viper.AddConfigPath(".")
+    viper.AutomaticEnv()
+
+    if err := viper.ReadInConfig(); err != nil {
+        log.Fatalf("Error reading config file, %s", err)
+    }
+}
+
+func main() {
+    initConfig()
+
+    fmt.Println("App Port:", viper.GetString("app.port"))
+    fmt.Println("Debug Mode:", viper.GetBool("app.debug"))
+}
